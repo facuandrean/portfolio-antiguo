579d9b3d2228722cc60ab597eefb0638bc75139b
facuandrean <facundoandrean22@gmail.com>
2023-04-27 13:08:52 -0300

    sección de contacto terminada (primera versión)

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const $navDesktop = d.querySelector('.header-nav__ul-desktop');
 const $tarjetasPortfolio = d.querySelectorAll('.main-containerTarjetas__tarjetaPortfolio');
 const $modalPortfolio = d.querySelectorAll('.modal');
 
+const $form_validacion = d.querySelector('.contact-form'), $inputs = d.querySelectorAll('.contact-form [required]');
+
 function modalPortfolio(idTarjeta) {
     $modalPortfolio.forEach(modal => {
         if (modal.className.includes(idTarjeta)) {
@@ -40,6 +42,14 @@ d.addEventListener('DOMContentLoaded', (e) => {
         $navDesktop.classList.add('none');
         $btnHamburger.classList.remove('none');
     }
+
+    $inputs.forEach(input => {
+        const $span = d.createElement('span');
+        $span.id = input.name;
+        $span.textContent = input.title;
+        $span.classList.add('contact-form-error', 'none');
+        input.insertAdjacentElement('afterend', $span);
+    });
 });
 
 w.addEventListener('resize', (e) => {
@@ -81,3 +91,68 @@ d.addEventListener('click', (e) => {
     }
     
 });
+
+document.addEventListener('submit', (e) => {
+
+    if (e.target === $form_validacion) {
+
+        const $loader = d.querySelector('.contact-form-loader'), $response = d.querySelector('.contact-form-response');
+
+        $loader.classList.remove('none');
+
+        // setTimeout(() => {
+        //     $loader.classList.add('none-2');
+        //     $response.classList.remove('none-2');
+        //     $form_validacion.reset();
+
+        //     setTimeout(() => {
+        //         $response.classList.add('none-2');
+        //     }, 3000);
+        // }, 3000);
+
+        // comentamos este código porque va a ser reemplazado por una respuesta desde el lado del servidor por una petición fetch.
+
+        fetch("https://formsubmit.co/ajax/facundoandrean22@gmail.com", {
+            method: "POST",
+            body: new FormData(e.target)
+            // el formdata parsea todos los elementos que traiga el formulario. Y para que haga ese parseo por nosotros, todos los inputs deben de tener su atributo name establecido porque es el nombre que toma la variable cuando mandamos el formulario.
+        })
+        .then(res => res.ok ? res.json() : Promise.reject(res))
+        .then(json => { 
+            console.log(json);
+            $loader.classList.add('none');
+            $response.classList.remove('none');
+            $response.innerHTML = `<p>${json.message}</p>`
+            $form_validacion.reset(); 
+        })
+        .catch(err => {
+            console.log(err);
+            let message = err.statusText || 'Ocurrió un error al enviar, intenta nuevamente.';
+            $response.innerHTML = `<p>Error: ${err.status}: ${message}</p>`;
+        })
+        .finally(() => setTimeout(() => {
+            $response.classList.add('none');
+            $response.innerHTML = "";
+        }, 3000))
+    }
+
+})
+
+document.addEventListener('keyup', (e) => {
+
+    if (e.target.matches('.contact-form [required]')) {
+        let $input = e.target, pattern = $input.pattern || $input.dataset.pattern;
+
+        if (pattern && $input.value !== "") {
+            let regex = new RegExp(pattern);
+            return !regex.exec($input.value) ? d.getElementById($input.name).classList.add('is-active') : d.getElementById($input.name).classList.remove('is-active')
+            // si el valor del input no cumple con la expresión regular, se ejecuta ?, pero si cumple, se ejecuta :
+        }
+
+        if (!pattern) {
+            return $input.value === "" ? d.getElementById($input.name).classList.add('is-active') : d.getElementById($input.name).classList.remove('is-active')
+            // si el input value es exactamente igual a nada, muestra el mensaje de error, sino no lo muestres.
+        }
+    }
+
+})
